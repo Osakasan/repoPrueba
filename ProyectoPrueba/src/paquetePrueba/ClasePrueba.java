926ce525b4124b04f4403a779bb880ea55c5123d
Antonioe <depressed.osaka@gmail.com>
2019-02-08 09:07:04 +0100

    lorele-i-uh

--- ProyectoPrueba/src/paquetePrueba/ClasePrueba.java
+++ ProyectoPrueba/src/paquetePrueba/ClasePrueba.java
@@ -4,6 +4,7 @@ public class ClasePrueba {
 	public static void main(String[]a) {
 		System.out.println("Esta es una prueba para subir a git");
 		System.out.println("A ver si funciona esto de una vez");
+		System.out.println("Y por fin funciona. Podriamos usar esto en Programacion, eh");
 	}
 
 }
